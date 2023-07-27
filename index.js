1fc06baef7d7590401f47c0579f3fdb547abd316
Khaled <khaledsahli36@gmail.com>
2023-07-27 12:47:34 +0100

    SSL

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('https')
+const fs = require('fs')
+
+const options = {
+  key: fs.readFileSync('cert/key.pem'),
+  cert: fs.readFileSync('cert/cert.pem'),
+}
+
+http
+  .createServer(options, (req, res) => {
+    res.end('SSL')
+  })
+  .listen(8520)
